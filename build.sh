d6480f362723c83139a6be2cb8182ccd461fce5e
Joshua Clarke <josh@transhumancreative.com>
2021-03-07 14:45:43 -0500

    Finish conversion to TypeScript

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ then
     exit
 fi
 
-pkg --out-path ./build/propass-win -t node15-win ./dist/propass.js && \
-pkg --out-path ./build/propass-mac -t node15-macos ./dist/propass.js && \
-pkg --out-path ./build/propass-lin -t node15-linux ./dist/propass.js && \
+pkg --out-path ./build/propass-win -t latest-win ./dist/propass.js && \
+pkg --out-path ./build/propass-mac -t latest-macos ./dist/propass.js && \
+pkg --out-path ./build/propass-lin -t latest-linux ./dist/propass.js && \
 zip -j ./build/propass-win/propass-windows.zip ./build/propass-win/propass.exe README.md LICENSE && \
 zip -j ./build/propass-mac/propass-macos.zip ./build/propass-mac/propass README.md LICENSE && \
 zip -j ./build/propass-lin/propass-linux.zip ./build/propass-lin/propass README.md LICENSE && \
