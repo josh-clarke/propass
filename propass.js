91e4096e0a16e0a721dfabd106702d7d00a54db5
Joshua Clarke <josh@transhumancreative.com>
2021-03-06 22:47:46 -0500

    TS compiler does not error out when converting

--- propass.js
+++ propass.js
@@ -1,269 +1,249 @@
 #!/usr/bin/env node
-
+"use strict";
+exports.__esModule = true;
 // Imports
-const { strict } = require('yargs')
-const yargs = require('yargs')
-const rand = require('random-seed').create()
-
-// Data
-const syllables = require('./syllables.json')
-
+var yargs = require("yargs");
+var uheprng = require("random-seed");
+// Variables
+var syllables = require('./syllables.json');
+var rand = uheprng.create();
 /**
  * Parse command line arguments
  */
-
 yargs
     .scriptName('propass')
     .usage('$0 <cmd> [args]')
     .example([
-        ['$0', 'Generates 3 password options with the defaults'],
-        ['$0 -l 18', 'Generates 3 password options wthat are 18 characters long'],
-        ['$0 -p 10 -s 2 -n 3 -c', 'Generates 10 password options with 2 symbols, 3 numbers, and capitals'],
-        ['$0 -p 5 -mc', 'Generates 5 password options with capital letters and the numbers and symbols mixed in']
-    ])
+    ['$0', 'Generates 3 password options with the defaults'],
+    ['$0 -l 18', 'Generates 3 password options wthat are 18 characters long'],
+    ['$0 -p 10 -s 2 -n 3 -c', 'Generates 10 password options with 2 symbols, 3 numbers, and capitals'],
+    ['$0 -p 5 -mc', 'Generates 5 password options with capital letters and the numbers and symbols mixed in']
+])
     .option('p', {
-        alias: 'passwords',
-        default: 3,
-        describe: 'How many password options',
-        type: 'number'
-    })
+    alias: 'passwords',
+    "default": 3,
+    describe: 'How many password options',
+    type: 'number'
+})
     .option('l', {
-        alias: 'lengthpass',
-        default: 13,
-        describe: 'Total characters in password',
-        type: 'number'
-    })
+    alias: 'lengthpass',
+    "default": 13,
+    describe: 'Total characters in password',
+    type: 'number'
+})
     .option('n', {
-        alias: 'numbers',
-        default: 2,
-        describe: 'Number characters in password',
-        type: 'number'
-    })
+    alias: 'numbers',
+    "default": 2,
+    describe: 'Number characters in password',
+    type: 'number'
+})
     .option('s', {
-        alias: 'symbols',
-        default: 1,
-        describe: 'Symbol characters in password',
-        type: 'number'
-    })
-    .option ('c', {
-        alias: 'caps',
-        default: false,
-        describe: 'Include capitals',
-        type: 'boolean'
-    })
-    .option ('m', {
-        alias: 'mix',
-        default: false,
-        describe: 'Mix the symbols and numbers randomly into the password',
-        type: 'boolean'
-    })
-    .option ('w', {
-        alias: 'unweighted',
-        default: false,
-        describe: 'Ignore the weighting for letter selection',
-        type: 'boolean'
-    })
+    alias: 'symbols',
+    "default": 1,
+    describe: 'Symbol characters in password',
+    type: 'number'
+})
+    .option('c', {
+    alias: 'caps',
+    "default": false,
+    describe: 'Include capitals',
+    type: 'boolean'
+})
+    .option('m', {
+    alias: 'mix',
+    "default": false,
+    describe: 'Mix the symbols and numbers randomly into the password',
+    type: 'boolean'
+})
+    .option('w', {
+    alias: 'unweighted',
+    "default": false,
+    describe: 'Ignore the weighting for letter selection',
+    type: 'boolean'
+})
     .requiresArg(['p', 'l', 'n', 's'])
     .strictOptions()
-    .check((argv, options) => {
-        if (argv.p < 1 || argv.p > 250) {
-            throw new Error('Please specify a number of passwords between 1 and 250.\n\n“Form is emptiness; emptiness is form. Emptiness is none other than form; form is none other than emptiness.” —The Heart Sutra')
-        } else if (argv.l < 10 || argv.l > 128) {
-            throw new Error('Please specify a password length of 10 or greater and up to 128.')
-        } else if (argv.n < 0) {
-            throw new Error('Please specify zero or a positive amount of number characters.')
-        }else if (argv.s < 0) {
-            throw new Error('Please specify zero or a positive amount of symbol characters.')
-        }else if (argv.n + argv.s > argv.l) {
-            throw new Error('Please specify a total amount of numbers and symbols are equal to or less than the total length of the password.')
-        }else if (isNaN(argv.p) || isNaN(argv.l) || isNaN(argv.n) || isNaN(argv.s) ) {
-            throw new Error('Only numbers are accepted as input values.')
-        }else{
-            return true;
-        }
-    })
+    .check(function (argv, options) {
+    if (argv.p < 1 || argv.p > 250) {
+        throw new Error('Please specify a number of passwords between 1 and 250.\n\n“Form is emptiness; emptiness is form. Emptiness is none other than form; form is none other than emptiness.” —The Heart Sutra');
+    }
+    else if (argv.l < 10 || argv.l > 128) {
+        throw new Error('Please specify a password length of 10 or greater and up to 128.');
+    }
+    else if (argv.n < 0) {
+        throw new Error('Please specify zero or a positive amount of number characters.');
+    }
+    else if (argv.s < 0) {
+        throw new Error('Please specify zero or a positive amount of symbol characters.');
+    }
+    else if (argv.n + argv.s > argv.l) {
+        throw new Error('Please specify a total amount of numbers and symbols are equal to or less than the total length of the password.');
+    }
+    else if (isNaN(argv.p) || isNaN(argv.l) || isNaN(argv.n) || isNaN(argv.s)) {
+        throw new Error('Only numbers are accepted as input values.');
+    }
+    else {
+        return true;
+    }
+})
     .showHelpOnFail(false, 'Specify --help for available options')
     .help()
-    .argv
-
-generatePassword(yargs.argv.lengthpass, yargs.argv.numbers, yargs.argv.symbols, yargs.argv.passwords, yargs.argv.caps, yargs.argv.mix)
-    
-    
-
+    .argv;
+generatePassword(yargs.argv.lengthpass, yargs.argv.numbers, yargs.argv.symbols, yargs.argv.passwords, yargs.argv.caps, yargs.argv.mix);
 /**
  * Generates password based on values specified
  */
-
-function generatePassword(passwordLength, numbersLength, symbolsLength, optionsLength, caps = false, mix = false) {
-    
-    let passwordOptions = ''
-    let trimValue = numbersLength + symbolsLength
-
+function generatePassword(passwordLength, numbersLength, symbolsLength, optionsLength, caps, mix) {
+    var passwordOptions;
+    var trimValue = numbersLength + symbolsLength;
     // Password Options
-    for (o = 0; o < optionsLength; o++) {
-        let password = []
-        let word = passwordLength - trimValue
-        let precedence = rand(2)
-        
+    for (var o = 0; o < optionsLength; o++) {
+        var password = [];
+        var word = passwordLength - trimValue;
+        var precedence = rand(2);
         // Word Creator
-        for (x = 0; x < word; x++) {
-            
-            if (x < 2 | x > word - 4 ) {
-                password.push(doubleSegment(caps, precedence))
-                x = x + 1
-            } else if (x >= 2 && x <= word - 4) {
+        for (var x = 0; x < word; x++) {
+            if (x < 2 || x > word - 4) {
+                password.push(doubleSegment(caps, precedence));
+                x = x + 1;
+            }
+            else if (x >= 2 && x <= word - 4) {
                 if ((word - x) % 3 == 0) {
-                    password.push(tripleSegment(caps, precedence))
-                    x = x + 2
-                } else if ((word - x) % 4 == 0) {
-                    let segment = ''
-                    let doubleOrQuadruple = rand(2)
-                    
-                    if(doubleOrQuadruple == 0) {
-                        segment = doubleSegment(caps, precedence)
-                        segment += doubleSegment(caps, precedence)
-                    }else{
-                        segment = quadrupleSegment(caps, precedence)
+                    password.push(tripleSegment(caps, precedence));
+                    x = x + 2;
+                }
+                else if ((word - x) % 4 == 0) {
+                    var segment = '';
+                    var doubleOrQuadruple = rand(2);
+                    if (doubleOrQuadruple == 0) {
+                        segment = doubleSegment(caps, precedence);
+                        segment += doubleSegment(caps, precedence);
                     }
-
-                    password.push(segment)
-                    x = x + 3
-                } else if ((word - x) % 2 == 0) {
-                    let segment = ''
-                    let doubleOrVowels = rand(2)
-
-                    if(doubleOrVowels == 0) {
-                        segment = doubleSegment(caps, precedence)
-                    }else{
-                        segment = setLetter(syllables.doubleVowels)
+                    else {
+                        segment = quadrupleSegment(caps, precedence);
                     }
-
-                    password.push(segment)
-                    x = x + 1
-                } else if ((word - x) % 1 == 0) {
+                    password.push(segment);
+                    x = x + 3;
+                }
+                else if ((word - x) % 2 == 0) {
+                    var segment = '';
+                    var doubleOrVowels = rand(2);
+                    if (doubleOrVowels == 0) {
+                        segment = doubleSegment(caps, precedence);
+                    }
+                    else {
+                        segment = setLetter(syllables.doubleVowels);
+                    }
+                    password.push(segment);
+                    x = x + 1;
+                }
+                else if ((word - x) % 1 == 0) {
                     // If it's prime!
-                    let segment = setLetter(syllables.singleVowels)
-
-                    password.push(segment)
+                    var segment = setLetter(syllables.singleVowels);
+                    password.push(segment);
                 }
             }
         }
-
         // Symbol Creator        
-        for(z = 0; z < symbolsLength; z++) {
-            let randomSymbols = rand(syllables.symbols.length)
-
-            password.push(syllables.symbols[randomSymbols])
+        for (var z = 0; z < symbolsLength; z++) {
+            var randomSymbols = rand(syllables.symbols.length);
+            password.push(syllables.symbols[randomSymbols]);
         }
-
         // Number Creator
-        for (y = 0; y < numbersLength; y++) {
-            let randomNumbers = rand(syllables.numbers.length)
-
-            password.push(syllables.numbers[randomNumbers])
+        for (var y = 0; y < numbersLength; y++) {
+            var randomNumbers = rand(syllables.numbers.length);
+            password.push(syllables.numbers[randomNumbers]);
         }
-
-        if(mix) {
-            shuffledPass = shuffleArray(password)
-            passwordOptions += shuffledPass.join('') + '\n'
-        }else{
-            passwordOptions += password.join('') + '\n'
+        if (mix) {
+            var shuffledPass = shuffleArray(password);
+            passwordOptions += shuffledPass.join('') + '\n';
+        }
+        else {
+            passwordOptions += password.join('') + '\n';
         }
     }
-
-    console.log(passwordOptions)
+    console.log(passwordOptions);
 }
-
 function doubleSegment(caps, precedence) {
-    let consonant = capConsonant(setLetter(syllables.singleConsonants), caps);
-    let vowel = setLetter(syllables.singleVowels)
-    let segment = '';
-
-    if(precedence) {
-        segment += consonant
-        segment += vowel
-    }else{                    
-        segment += vowel
-        segment += consonant
+    var consonant = capConsonant(setLetter(syllables.singleConsonants), caps);
+    var vowel = setLetter(syllables.singleVowels);
+    var segment = '';
+    if (precedence) {
+        segment += consonant;
+        segment += vowel;
     }
-    return segment
+    else {
+        segment += vowel;
+        segment += consonant;
+    }
+    return segment;
 }
-
 function tripleSegment(caps, precedence) {
-    let consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
-    let vowel = setLetter(syllables.singleVowels)
-    let segment = '';
-
-    if(precedence) {
-        segment += consonant
-        segment += vowel
-    }else{
-        segment += vowel
-        segment += consonant                        
+    var consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
+    var vowel = setLetter(syllables.singleVowels);
+    var segment = '';
+    if (precedence) {
+        segment += consonant;
+        segment += vowel;
     }
-    return segment
+    else {
+        segment += vowel;
+        segment += consonant;
+    }
+    return segment;
 }
-
 function quadrupleSegment(caps, precedence) {
-    let consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
-    let vowel = setLetter(syllables.doubleVowels)
-    let segment = '';
-
-    if(precedence) {
-        segment += consonant
-        segment += vowel
-    }else{
-        segment += vowel
-        segment += consonant
+    var consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
+    var vowel = setLetter(syllables.doubleVowels);
+    var segment = '';
+    if (precedence) {
+        segment += consonant;
+        segment += vowel;
     }
-    return segment
+    else {
+        segment += vowel;
+        segment += consonant;
+    }
+    return segment;
 }
-
 function capConsonant(consonant, caps) {
-    if(consonant.length < 2) {
-        return consonant = caps ? consonant.toUpperCase() : consonant
-    }else{
-        return consonant = caps ? consonant.charAt(0).toUpperCase() + consonant.slice(1) : consonant
+    if (consonant.length < 2) {
+        return consonant = caps ? consonant.toUpperCase() : consonant;
+    }
+    else {
+        return consonant = caps ? consonant.charAt(0).toUpperCase() + consonant.slice(1) : consonant;
     }
 }
-
 function shuffleArray(arr) {
-    let id = arr.length
+    var id = arr.length;
     while (0 !== id) {
-        let randomId = rand(id)
-        id -= 1
-
-        let tmp = arr[id]
-        arr[id] = arr[randomId]
-        arr[randomId] = tmp
+        var randomId = rand(id);
+        id -= 1;
+        var tmp = arr[id];
+        arr[id] = arr[randomId];
+        arr[randomId] = tmp;
     }
-    return arr
+    return arr;
     // https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
 }
-
 function setLetter(letterOptions) {
-    let isChecking = true
-    let letter = ''
-    let blacklist = []
-
+    var isChecking = true;
+    var letter = '';
+    var blacklist = [];
     while (isChecking) {
-        let thisOption = rand(letterOptions.length)
-        let letterSet = letterOptions[thisOption]
-
-        if(weightCheck(letterSet[1])) {
-            letter = letterSet[0]
-            isChecking = false
+        var thisOption = rand(letterOptions.length);
+        var letterSet = letterOptions[thisOption];
+        if (weightCheck(letterSet[1])) {
+            letter = letterSet[0];
+            isChecking = false;
         }
     }
-    return letter
+    return letter;
 }
-
 function weightCheck(weight) {
-    let d20 = rand(20) + 1
-
+    var d20 = rand(20) + 1;
     if (yargs.argv.unweighted || d20 >= weight) {
-        return true
+        return true;
     }
-
-    return false
+    return false;
 }
---
+++ propass.ts
@@ -0,0 +1,278 @@
+#!/usr/bin/env node
+
+// Imports
+import yargs = require('yargs')
+import uheprng = require('random-seed')
+
+interface Syllables {
+    'singleConsonants': Array<Array<{'char': string, 'weight': string}>>,
+    'doubleConsonants': Array<Array<{'char': string, 'weight': string}>>,
+    'singleVowels': Array<Array<{'char': string, 'weight': string}>>,
+    'doubleVowels': Array<Array<{'char': string, 'weight': string}>>,
+    'numbers': Array<string>,
+    'symbols': Array<string>
+}
+
+// Variables
+const syllables: Syllables = require('./syllables.json')
+const rand = uheprng.create()
+
+/**
+ * Parse command line arguments
+ */
+
+yargs
+    .scriptName('propass')
+    .usage('$0 <cmd> [args]')
+    .example([
+        ['$0', 'Generates 3 password options with the defaults'],
+        ['$0 -l 18', 'Generates 3 password options wthat are 18 characters long'],
+        ['$0 -p 10 -s 2 -n 3 -c', 'Generates 10 password options with 2 symbols, 3 numbers, and capitals'],
+        ['$0 -p 5 -mc', 'Generates 5 password options with capital letters and the numbers and symbols mixed in']
+    ])
+    .option('p', {
+        alias: 'passwords',
+        default: 3,
+        describe: 'How many password options',
+        type: 'number'
+    })
+    .option('l', {
+        alias: 'lengthpass',
+        default: 13,
+        describe: 'Total characters in password',
+        type: 'number'
+    })
+    .option('n', {
+        alias: 'numbers',
+        default: 2,
+        describe: 'Number characters in password',
+        type: 'number'
+    })
+    .option('s', {
+        alias: 'symbols',
+        default: 1,
+        describe: 'Symbol characters in password',
+        type: 'number'
+    })
+    .option ('c', {
+        alias: 'caps',
+        default: false,
+        describe: 'Include capitals',
+        type: 'boolean'
+    })
+    .option ('m', {
+        alias: 'mix',
+        default: false,
+        describe: 'Mix the symbols and numbers randomly into the password',
+        type: 'boolean'
+    })
+    .option ('w', {
+        alias: 'unweighted',
+        default: false,
+        describe: 'Ignore the weighting for letter selection',
+        type: 'boolean'
+    })
+    .requiresArg(['p', 'l', 'n', 's'])
+    .strictOptions()
+    .check((argv, options) => {
+        if (argv.p < 1 || argv.p > 250) {
+            throw new Error('Please specify a number of passwords between 1 and 250.\n\n“Form is emptiness; emptiness is form. Emptiness is none other than form; form is none other than emptiness.” —The Heart Sutra')
+        } else if (argv.l < 10 || argv.l > 128) {
+            throw new Error('Please specify a password length of 10 or greater and up to 128.')
+        } else if (argv.n < 0) {
+            throw new Error('Please specify zero or a positive amount of number characters.')
+        }else if (argv.s < 0) {
+            throw new Error('Please specify zero or a positive amount of symbol characters.')
+        }else if (argv.n + argv.s > argv.l) {
+            throw new Error('Please specify a total amount of numbers and symbols are equal to or less than the total length of the password.')
+        }else if (isNaN(argv.p) || isNaN(argv.l) || isNaN(argv.n) || isNaN(argv.s) ) {
+            throw new Error('Only numbers are accepted as input values.')
+        }else{
+            return true;
+        }
+    })
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .help()
+    .argv
+
+generatePassword(yargs.argv.lengthpass, yargs.argv.numbers, yargs.argv.symbols, yargs.argv.passwords, yargs.argv.caps, yargs.argv.mix)
+    
+    
+
+/**
+ * Generates password based on values specified
+ */
+
+function generatePassword(passwordLength, numbersLength, symbolsLength, optionsLength, caps, mix) {
+    
+    let passwordOptions: string
+    let trimValue: number = numbersLength + symbolsLength
+
+    // Password Options
+    for (let o:number = 0; o < optionsLength; o++) {
+        let password: Array<string> = []
+        let word: number = passwordLength - trimValue
+        let precedence: number = rand(2)
+        
+        // Word Creator
+        for (let x:number = 0; x < word; x++) {
+            
+            if (x < 2 || x > word - 4 ) {
+                password.push(doubleSegment(caps, precedence))
+                x = x + 1
+            } else if (x >= 2 && x <= word - 4) {
+                if ((word - x) % 3 == 0) {
+                    password.push(tripleSegment(caps, precedence))
+                    x = x + 2
+                } else if ((word - x) % 4 == 0) {
+                    let segment = ''
+                    let doubleOrQuadruple = rand(2)
+                    
+                    if(doubleOrQuadruple == 0) {
+                        segment = doubleSegment(caps, precedence)
+                        segment += doubleSegment(caps, precedence)
+                    }else{
+                        segment = quadrupleSegment(caps, precedence)
+                    }
+
+                    password.push(segment)
+                    x = x + 3
+                } else if ((word - x) % 2 == 0) {
+                    let segment = ''
+                    let doubleOrVowels = rand(2)
+
+                    if(doubleOrVowels == 0) {
+                        segment = doubleSegment(caps, precedence)
+                    }else{
+                        segment = setLetter(syllables.doubleVowels)
+                    }
+
+                    password.push(segment)
+                    x = x + 1
+                } else if ((word - x) % 1 == 0) {
+                    // If it's prime!
+                    let segment = setLetter(syllables.singleVowels)
+
+                    password.push(segment)
+                }
+            }
+        }
+
+        // Symbol Creator        
+        for(let z:number = 0; z < symbolsLength; z++) {
+            let randomSymbols = rand(syllables.symbols.length)
+
+            password.push(syllables.symbols[randomSymbols])
+        }
+
+        // Number Creator
+        for (let y:number = 0; y < numbersLength; y++) {
+            let randomNumbers = rand(syllables.numbers.length)
+
+            password.push(syllables.numbers[randomNumbers])
+        }
+
+        if(mix) {
+            let shuffledPass:Array<string> = shuffleArray(password)
+            passwordOptions += shuffledPass.join('') + '\n'
+        }else{
+            passwordOptions += password.join('') + '\n'
+        }
+    }
+
+    console.log(passwordOptions)
+}
+
+function doubleSegment(caps, precedence) {
+    let consonant = capConsonant(setLetter(syllables.singleConsonants), caps);
+    let vowel = setLetter(syllables.singleVowels)
+    let segment = '';
+
+    if(precedence) {
+        segment += consonant
+        segment += vowel
+    }else{                    
+        segment += vowel
+        segment += consonant
+    }
+    return segment
+}
+
+function tripleSegment(caps, precedence) {
+    let consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
+    let vowel = setLetter(syllables.singleVowels)
+    let segment = '';
+
+    if(precedence) {
+        segment += consonant
+        segment += vowel
+    }else{
+        segment += vowel
+        segment += consonant                        
+    }
+    return segment
+}
+
+function quadrupleSegment(caps, precedence) {
+    let consonant = capConsonant(setLetter(syllables.doubleConsonants), caps);
+    let vowel = setLetter(syllables.doubleVowels)
+    let segment = '';
+
+    if(precedence) {
+        segment += consonant
+        segment += vowel
+    }else{
+        segment += vowel
+        segment += consonant
+    }
+    return segment
+}
+
+function capConsonant(consonant, caps) {
+    if(consonant.length < 2) {
+        return consonant = caps ? consonant.toUpperCase() : consonant
+    }else{
+        return consonant = caps ? consonant.charAt(0).toUpperCase() + consonant.slice(1) : consonant
+    }
+}
+
+function shuffleArray(arr) {
+    let id = arr.length
+    while (0 !== id) {
+        let randomId = rand(id)
+        id -= 1
+
+        let tmp = arr[id]
+        arr[id] = arr[randomId]
+        arr[randomId] = tmp
+    }
+    return arr
+    // https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
+}
+
+function setLetter(letterOptions) {
+    let isChecking = true
+    let letter = ''
+    let blacklist = []
+
+    while (isChecking) {
+        let thisOption = rand(letterOptions.length)
+        let letterSet = letterOptions[thisOption]
+
+        if(weightCheck(letterSet[1])) {
+            letter = letterSet[0]
+            isChecking = false
+        }
+    }
+    return letter
+}
+
+function weightCheck(weight) {
+    let d20 = rand(20) + 1
+
+    if (yargs.argv.unweighted || d20 >= weight) {
+        return true
+    }
+
+    return false
+}
